e24a05fd5d870052dbf99a942987b7cd7ced73ee
douglas-silvaARCADIS <90846472+douglas-silvaARCADIS@users.noreply.github.com>
2022-12-04 22:31:02 -0300

    Configuracao SP

---
+++ sharepoint_upload.py
@@ -0,0 +1,17 @@
+import requests
+from shareplum import Office365
+#from config import config
+
+#Informações de configuração
+
+username = ""
+password = ""
+path_name = 'https://arcadiso365.sharepoint.com'
+site_name = "s"
+link  = "https://arcadiso365.sharepoint.com/:f:/r/sites/CorporativoBR/acervotecnico/PISF/Teste"
+
+authcookie = Office365(path_name,username=username,password=password).GetCookies()
+session = requests.Session()
+session.cookies = authcookie
+
+
